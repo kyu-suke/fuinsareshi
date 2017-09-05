0bdd0022974598ef18f9ed2e2e24ed59a049c5f5
kyu-suke <ykaneoka7@gmail.com>
2017-09-05 19:48:10 +0900

    [fix]glide.ymlのgoproxyのバージョン指定を変更

--- service/service.go
+++ service/service.go
@@ -3,13 +3,19 @@ package service
 import (
 	"net/http"
 	"github.com/elazarl/goproxy"
-	"github.com/labstack/gommon/log"
+	//"github.com/labstack/gommon/log"
+	"log"
 )
 
 func Proxy() {
-	verbose := false
-	addr := ":8080"
+//	verbose := false
+//	addr := ":8080"
+//	proxy := goproxy.NewProxyHttpServer()
+//	proxy.Verbose = verbose
+//	log.Fatal(http.ListenAndServe(addr, proxy))
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = verbose
-	log.Fatal(http.ListenAndServe(addr, proxy))
+	proxy.Verbose = true
+
+	log.Fatal(http.ListenAndServe(":8080", proxy))
+
 }
