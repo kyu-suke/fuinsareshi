134d1874a8e8028d4dda0c18d6baec5bbf10bfef
kyu-suke <ykaneoka7@gmail.com>
2017-09-11 16:22:41 +0900

    httputilを利用

--- service/service.go
+++ service/service.go
@@ -1,10 +1,15 @@
 package service
 
 import (
-	"net/http"
-	"github.com/elazarl/goproxy"
+	//"github.com/elazarl/goproxy"
 	//"github.com/labstack/gommon/log"
+
 	"log"
+	"net/http"
+	"net/http/httputil"
+
+	"io/ioutil"
+	"fmt"
 )
 
 func Proxy() {
@@ -13,9 +18,30 @@ func Proxy() {
 //	proxy := goproxy.NewProxyHttpServer()
 //	proxy.Verbose = verbose
 //	log.Fatal(http.ListenAndServe(addr, proxy))
-	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
+//	proxy := goproxy.NewProxyHttpServer()
+//	proxy.Verbose = true
+//	proxy.NewConnectDialToProxy()
+//	log.Fatal(http.ListenAndServe(":8080", proxy))
 
-	log.Fatal(http.ListenAndServe(":8080", proxy))
+	director := func(request *http.Request) {
+		request.URL.Scheme = "http"
+		request.URL.Host = ":80"
+	}
+	modifyResponse := func(response *http.Response) error {
+		r, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(r))
+		return nil
+	}
+	rp := &httputil.ReverseProxy{
+		Director: director,
+		ModifyResponse: modifyResponse,
+	}
+	server := http.Server{
+		Addr:    ":9000",
+		Handler: rp,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err.Error())
+	}
 
 }
